dd63e1aebad9f151a7efe520db38361365a241fc
git4x <39692157+git4x@users.noreply.github.com>
2024-12-17 08:07:12 +0000

    initial

---
+++ vite-starter/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log('render', Math.random())
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount((prev) => {
+      console.log("inside......" + prev)
+      return prev + 1
+    })
+  }
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick}>
+          count is {count}
+        </button>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
